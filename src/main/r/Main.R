8c8f3afc966570582dd91b315660e44ac0f2acf2
m168 <6865656+maciej168@users.noreply.github.com>
2017-06-01 21:42:44 +0200

    colnames

--- src/main/r/Main.R
+++ src/main/r/Main.R
@@ -2,8 +2,6 @@ library(foreign)
 
 MyData <- read.arff(file="../resources/1year.arff")
 
-
-print(as.list(read.csv(file="../resources/attributes.csv", sep=",", header=FALSE)))
 colnames(MyData) <- read.csv(file="../resources/attributes.csv", sep=",", header=FALSE)[,1]
 print(MyData[1,])
 

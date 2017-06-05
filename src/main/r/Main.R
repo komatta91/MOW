e411268d7432ec673024b48fa124b5615adf262b
m168 <6865656+maciej168@users.noreply.github.com>
2017-06-05 23:44:07 +0200

    c

--- src/main/r/Main.R
+++ src/main/r/Main.R
@@ -2,23 +2,29 @@
 library(foreign)
 
 
+AllTheData <- list(
+    read.arff(file="../resources/polishcompanies/1year.arff"),
+    read.arff(file="../resources/polishcompanies/2year.arff"),
+    read.arff(file="../resources/polishcompanies/3year.arff"),
+    read.arff(file="../resources/polishcompanies/4year.arff"),
+    read.arff(file="../resources/polishcompanies/5year.arff"),
+    read.csv(file="../resources/dota2/dota2Test.csv", sep=",", header=FALSE),
+    read.csv(file="../resources/dota2/dota2Train.csv", sep=",", header=FALSE)
+)
 
-MyData <- read.arff(file="../resources/polishcompanies/1year.arff")
-colnames(MyData) <- read.csv(file="../resources/polishcompanies/attributes.csv", sep=",", header=FALSE)[,1]
+lapply(AllTheData, function(Data){
+    unmanaged <- Data[,1:ncol(Data)-1]
+    managed <- Data
+    labels <- c(Data[,ncol(Data)])
 
-unmanaged <- MyData[,1:ncol(MyData)-1]
-managed <- MyData
-labels <- c(MyData[,'CLASSIFICATION'])
+    #source("EM.R")
+    #em1 = emFunction1(dataset=unmanaged, labels=labels)
 
-ograniczony <- unmanaged[6500:7000,]
+    source("IsolationForest.R")
+    samplesPerTree <- 700
+    anomalyTreshold <- 0.01
+    isolationForest <- iForest(X=unmanaged, numberOfTrees=10, samplesPerTree=samplesPerTree, multicore=FALSE)
+    anomalies <- sum(unlist(lapply(isolationForest$forest[], function(tree) Filter(function(size) size > 0 && size < anomalyTreshold*samplesPerTree, tree[, 'Size']))))
+    print(c(nrow(unmanaged)-anomalies, anomalies))
+})
 
-
-#source("EM.R")
-#em1 = emFunction1(dataset=unmanaged, labels=labels)
-
-source("IsolationForest.R")
-samplesPerTree <- 700
-anomalyTreshold <- 0.01
-isolationForest <- iForest(X=unmanaged, numberOfTrees=10, samplesPerTree=samplesPerTree, multicore=FALSE)
-anomalies <- sum(unlist(lapply(isolationForest$forest[], function(tree) Filter(function(size) size > 0 && size < anomalyTreshold*samplesPerTree, tree[, 'Size']))))
-print(anomalies)

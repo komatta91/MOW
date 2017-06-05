7f3c166136ef3739873e1e37fe5c61a04194c5e7
MrRadziu <radek.skarzycki@gmail.com>
2017-06-05 23:04:38 +0200

    No unknown data

--- src/main/r/Main.R
+++ src/main/r/Main.R
@@ -3,8 +3,8 @@ library(foreign)
 
 
 
-MyData <- read.arff(file="../resources/1year.arff")
-colnames(MyData) <- read.csv(file="../resources/attributes.csv", sep=",", header=FALSE)[,1]
+MyData <- read.arff(file="../resources/polishcompanies/1year.arff")
+colnames(MyData) <- read.csv(file="../resources/polishcompanies/attributes.csv", sep=",", header=FALSE)[,1]
 
 unmanaged <- MyData[,1:ncol(MyData)-1]
 managed <- MyData
@@ -13,13 +13,12 @@ labels <- c(MyData[,'CLASSIFICATION'])
 ograniczony <- unmanaged[6500:7000,]
 
 
-# source("EM.R")
-# em1 = emFunction1(dataset=unmanaged, labels=labels)
-# em2 = emFunction2(dataset=unmanaged, labels=labels)
+#source("EM.R")
+#em1 = emFunction1(dataset=unmanaged, labels=labels)
 
 source("IsolationForest.R")
 samplesPerTree <- 700
-anomalyTreshold <- 0.1
+anomalyTreshold <- 0.01
 isolationForest <- iForest(X=unmanaged, numberOfTrees=10, samplesPerTree=samplesPerTree, multicore=FALSE)
 anomalies <- sum(unlist(lapply(isolationForest$forest[], function(tree) Filter(function(size) size > 0 && size < anomalyTreshold*samplesPerTree, tree[, 'Size']))))
 print(anomalies)

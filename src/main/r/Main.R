9ca3323e164abe39401e3bcb3d63e6c204f80c09
m168 <6865656+maciej168@users.noreply.github.com>
2017-06-05 21:38:19 +0200

    c

--- src/main/r/Main.R
+++ src/main/r/Main.R
@@ -13,9 +13,13 @@ labels <- c(MyData[,'CLASSIFICATION'])
 ograniczony <- unmanaged[6500:7000,]
 
 
-source("EM.R")
-em1 = emFunction1(dataset=unmanaged, labels=labels)
-em2 = emFunction2(dataset=unmanaged, labels=labels)
+# source("EM.R")
+# em1 = emFunction1(dataset=unmanaged, labels=labels)
+# em2 = emFunction2(dataset=unmanaged, labels=labels)
 
 source("IsolationForest.R")
-isolationForest <- iForest(X=unmanaged, nt=10, phi=700, multicore=FALSE)
+samplesPerTree <- 700
+anomalyTreshold <- 0.1
+isolationForest <- iForest(X=unmanaged, numberOfTrees=10, samplesPerTree=samplesPerTree, multicore=FALSE)
+anomalies <- sum(unlist(lapply(isolationForest$forest[], function(tree) Filter(function(size) size > 0 && size < anomalyTreshold*samplesPerTree, tree[, 'Size']))))
+print(anomalies)
